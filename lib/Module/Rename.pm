3f7e35db83ee2f9ff1333187adbce1f53a064625
mschilli <github@perlmeister.com>
2005-03-28 01:38:29 +0000

    more tests

--- lib/Module/Rename.pm
+++ lib/Module/Rename.pm
@@ -17,13 +17,16 @@ sub new {
     my($class, %options) = @_;
 
     my $self = {
-        name_old    => undef,
-        name_new    => undef,
-        dir_exclude => ['blib'],
+        name_old           => undef,
+        name_new           => undef,
+        dir_exclude        => ['blib'],
+        dir_ignore         => ['CVS'],
+        wipe_empty_subdirs => 0,
         %options,
     };
 
     $self->{dir_exclude_hash} = { map { $_ => 1 } @{$self->{dir_exclude}} };
+    $self->{dir_ignore_hash}  = { map { $_ => 1 } @{$self->{dir_ignore}} };
 
     bless $self, $class;
 }
@@ -37,8 +40,13 @@ sub find_and_rename {
     (my $replace_by = $self->{name_new}) =~ s#::#/#g;
 
     my @files = ();
+    my %empty_subdirs = ();
 
     find(sub {
+        if(-d and $self->dir_empty($_)) {
+            WARN "$File::Find::name is an empty subdir";
+            $empty_subdirs{$File::Find::name}++;
+        }
         if(-d and exists $self->{dir_exclude_hash}->{$_}) {
             $File::Find::prune = 1;
             return;
@@ -55,6 +63,49 @@ sub find_and_rename {
         mkd $dir unless -d $dir;
         mv $file, $newfile;
     }
+
+    (my $dashed_look_for   = $self->{name_old}) =~ s#::#-#g;
+    (my $dashed_replace_by = $self->{name_new}) =~ s#::#-#g;
+
+        # Rename any top directory files like Foo-Bar-0.01
+    my @rename_candidates = ();
+    find(sub {
+        if(/$dashed_look_for/) {
+            push @rename_candidates, $File::Find::name;
+        }
+    }, $start_dir);
+    for my $item (@rename_candidates) {
+        (my $newitem = $item) =~ s/$dashed_look_for/$dashed_replace_by/;
+        mv $item, $newitem;
+    }
+        # Update empty_subdirs with the latest name changes
+    %empty_subdirs = map { s/$dashed_look_for/$dashed_replace_by/; $_; }
+        %empty_subdirs;
+
+    my @dirs = ();
+        # Delete all empty dirs
+    finddepth(sub { 
+        if(-d and $self->dir_empty($_) and
+           ! exists $empty_subdirs{$File::Find::name}) {
+            INFO "$File::Find::name is empty and can go away";
+            rmf $_ if $self->{wipe_empty_subdirs};
+            $File::Find::prune = 1;
+        }
+    }, $start_dir);
+}
+
+###########################################
+sub dir_empty {
+###########################################
+    my($self, $dir) = @_;
+
+    opendir DIR, $dir or LOGDIE "Cannot open dir $dir";
+    my @items = grep { $_ ne "." and $_ ne ".." } readdir DIR;
+    closedir DIR;
+
+    @items = grep { ! exists $self->{dir_ignore_hash}->{$_} } @items;
+    
+    return ! scalar @items;
 }
 
 ###########################################
@@ -98,11 +149,20 @@ Module::Rename - Utility functions for renaming a module distribution
 
 =head1 DESCRIPTION
 
-Module::Rename blah blah blah.
+Did you ever create a module distribution, only to realize later that
+the module hierarchary needed to be changed? All of a sudden, 
+C<Cool::Frobnicator> didn't sound cool anymore, but needed to be
+C<Util::Frobnicator>?
+
+        name_old           => undef,
+        name_new           => undef,
+        dir_exclude        => ['blib'],
+        dir_ignore         => ['CVS'],
+        wipe_empty_subdirs => 1,
 
 =head1 EXAMPLES
 
-  $ perl -MModule::Rename -le 'print $foo'
+  $ module-rename Cool::Frobnicator Util::Frobnicator Cool-Frobnicator-0.01
 
 =head1 LEGALESE
 
--- t/001Basic.t
+++ t/001Basic.t
@@ -12,7 +12,7 @@ use Log::Log4perl qw(:easy);
 use File::Basename;
 use File::Find;
 
-#Log::Log4perl->easy_init($DEBUG);
+Log::Log4perl->easy_init({level => $DEBUG, file => 'STDOUT'});
 
 BEGIN { use_ok('Module::Rename') };
 
@@ -25,18 +25,25 @@ rmf "tmp" if -d "tmp";
 cp_r("Foo-Bar", "tmp");
 
 my $ren = Module::Rename->new(
-    name_old => "Foo::Bar",
-    name_new => "Ka::Boom",
+    name_old           => "Foo::Bar",
+    name_new           => "Ka::Boom",
+    wipe_empty_subdirs => 1,
 );
 
 $ren->find_and_rename("tmp");
 
 ok(! -f "tmp/Foo-Bar/lib/Foo/Bar.pm", "Old file deleted");
-ok( -f "tmp/Foo-Bar/lib/Ka/Boom.pm", "File renamed");
+ok( -f "tmp/Ka-Boom/lib/Ka/Boom.pm", "File renamed");
 
-my $data = slurp "tmp/Foo-Bar/lib/Ka/Boom.pm";
+my $data = slurp "tmp/Ka-Boom/lib/Ka/Boom.pm";
 unlike($data, qr/Foo::Bar/, "Content renamed");
 like($data, qr/Ka::Boom/, "Content renamed");
+
+$data = slurp "tmp/Ka-Boom/Makefile.PL";
+unlike($data, qr/Foo::Bar/, "Content renamed");
+
+ok(-d   "tmp/Ka-Boom/eg",      "Leave previously empty dir untouched");
+ok(! -d "tmp/Ka-Boom/lib/Foo", "Sweep away now-empty subdir");
 
 rmf "tmp";
 
